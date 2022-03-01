50695749ec35020e3ef2ae8cabd5f53ff3c267b3
IlkinBairamov <ilkinabr@code.edu.az>
2022-03-01 06:37:02 -0800

    homework

---
+++ FrontToBack/Migrations/20220301111744_AddIsdeletedToTable.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FrontToBack.Migrations
+{
+    public partial class AddIsdeletedToTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Isdeleted",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Isdeleted",
+                table: "AspNetUsers");
+        }
+    }
+}
